c3d735800a1e4540e5a6565beede1ab7698ca45e
Alvin Zhao <alvin.zhao@myob.com>
2021-08-04 12:18:17 +1000

    private methods

--- payslip2/payslip.App/PayslipCalculator.cs
+++ payslip2/payslip.App/PayslipCalculator.cs
@@ -5,26 +5,6 @@ namespace PayslipGenerator
 {
 	public static class PayslipMethods
 	{
-		public static int GrossIncome(int salary) => salary / 12;
-
-		public static decimal TaxRate(int salary, TaxBracket[] taxBrackets) =>
-			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).TaxRate;
-
-		public static int FlatTax(int salary, TaxBracket[] taxBrackets) =>
-			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).FlatTax;
-
-		public static int SubtractionAmount(int salary, TaxBracket[] taxBrackets) =>
-			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).LowerLimit;
-
-		public static int IncomeTax(int salary, TaxBracket[] taxBrackets) =>
-			(int)Math.Ceiling((FlatTax(salary, taxBrackets) + (salary - SubtractionAmount(salary, taxBrackets)) * TaxRate(salary, taxBrackets)) / 12);
-
-		public static int NetIncome(int salary, TaxBracket[] taxBrackets) =>
-			GrossIncome(salary) - IncomeTax(salary, taxBrackets);
-
-		public static int Super(int salary, int superRate) =>
-			GrossIncome(salary) * superRate / 100;
-
 		public static Payslip GeneratePayslip(Data data, TaxBracket[] taxBrackets) =>
 			new Payslip(
 				$"{data.FirstName} {data.LastName}",
@@ -34,5 +14,25 @@ namespace PayslipGenerator
 				NetIncome(data.AnnualSalary, taxBrackets),
 				Super(data.AnnualSalary, data.SuperRate)
 			);
+
+		private static int GrossIncome(int salary) => salary / 12;
+
+		private static decimal TaxRate(int salary, TaxBracket[] taxBrackets) =>
+			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).TaxRate;
+
+		private static int FlatTax(int salary, TaxBracket[] taxBrackets) =>
+			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).FlatTax;
+
+		private static int SubtractionAmount(int salary, TaxBracket[] taxBrackets) =>
+			taxBrackets.Single(b => salary > b.LowerLimit && salary <= b.UpperLimit).LowerLimit;
+
+		private static int IncomeTax(int salary, TaxBracket[] taxBrackets) =>
+			(int)Math.Ceiling((FlatTax(salary, taxBrackets) + (salary - SubtractionAmount(salary, taxBrackets)) * TaxRate(salary, taxBrackets)) / 12);
+
+		private static int NetIncome(int salary, TaxBracket[] taxBrackets) =>
+			GrossIncome(salary) - IncomeTax(salary, taxBrackets);
+
+		private static int Super(int salary, int superRate) =>
+			GrossIncome(salary) * superRate / 100;
 	}
 }
